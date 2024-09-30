0725a521d08e1253908f8468d542a3a185d01720
alchemistOfWeb <nikitakzntcv@gmail.com>
2024-09-30 20:14:00 +0000

    FS-1: update output in readFile function and remove all extra comments in main.cpp

--- FS-1/main.cpp
+++ FS-1/main.cpp
@@ -7,14 +7,11 @@
 
 
 void readFile(char* filePath) {
-    // do smth...
     int fd = open(filePath, O_RDONLY);
     if (fd == -1) {
         perror("Error during creating file");
         exit(EXIT_FAILURE);
     }
-    
-    printf("file was opened\n");
 
     while(true) {    
         char buffer[BUFFER_SIZE];
@@ -27,19 +24,18 @@ void readFile(char* filePath) {
         if (bytes_read == 0) break;
      
         buffer[bytes_read] = '\0';
-        printf("\n%s\n", buffer);
+        printf("%s", buffer);
     }
-
+    printf("\n");
     close(fd);
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        // std::cerr << "Error: No file paht provided." <<std::endl;
         perror("Error: No file paht provided.");
         return 1;
     }
-    //std::string filePath = argv[1];
+
     readFile(argv[1]);
     return 0;
 }
